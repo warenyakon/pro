6f87192b480ca944624f06f0b8bafc70b3bbf073
fortty <warenyagon@gmail.com>
2020-01-29 14:31:05 +0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template:'<ejs-schedule height="850" width ="1000"></ejs-schedule>',
+  // templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'pro';
+}
+
+
+
